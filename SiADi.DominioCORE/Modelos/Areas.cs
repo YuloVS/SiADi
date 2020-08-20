a4a4b2fe5ca450e2ae18ae5c38e6dadb24ccf438
Giuliano Vallejos Santajuliana <vallejosgvs@gmail.com>
2020-08-20 12:35:20 -0300

    Agregadas las clases Areas, Asistencia, Cargo, Persona, y Tipo_usuario

---
+++ SiADi.DominioCORE/Modelos/Areas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiADi.DominioCORE.Modelos
+{
+    class Areas
+    {
+        public int id_area { set; get; }
+        public string descripcion { set; get; }
+    }
+}
---
+++ SiADi.DominioCORE/Modelos/Cargo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiADi.DominioCORE.Modelos
+{
+    class Cargo
+    {
+        public int id_cargo { set; get; }
+        public string descripcion { set; get; }
+        public DateTime horario_entrada { set; get; }
+        public DateTime horario_salida { set; get; }
+        public int id_area { set; get; }
+        public int nivel { set; get; }
+        public float salario { set; get; }
+
+    }
+}
