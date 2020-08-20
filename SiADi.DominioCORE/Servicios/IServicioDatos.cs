b86ec7dd357c02919bed28217d316a615e91ed65
Giuliano Vallejos Santajuliana <vallejosgvs@gmail.com>
2020-08-20 12:49:18 -0300

    Servicio de datos agregados.

---
+++ SiADi.DominioCORE/Servicios/IServicioDatos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiADi.DominioCORE.Servicios
+{
+    interface IServicioDatos<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Crear(T entidad);
+        Task<T> Actualizar(int id, T entidad);
+        Task<bool> Borrar(int id);
+    }
+}
