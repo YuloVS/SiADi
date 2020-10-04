48a2fcc20f32f1bc46708bf45642800c636df347
Giuliano Vallejos Santajuliana <vallejosgvs@gmail.com>
2020-10-04 11:09:40 -0300

    Inicio.cs normalizado.

--- SiADi/Inicio.cs
+++ SiADi/Inicio.cs
@@ -15,19 +15,19 @@ namespace SiADi
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Principal());
-            using (var db = new SiADiDB())
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Principal());
+            /*using (var db = new SiADiDB())
             {
-                db.Cargos.Add(new Cargo { Nombre = "Test Cargo", Salario = 12000});
+                db.Areas.Add(new Area { Nombre = "Test area", Descripcion = "Esta es una area de prueba"});
                 db.SaveChanges();
 
                 foreach (var cargo in db.Cargos)
                 {
                     Console.WriteLine(cargo.Nombre);
                 }
-            }
+            }*/
         }
     }
 }
