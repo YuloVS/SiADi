3edebed55ae828fc4845c5027680d8d4262e11c2
Giuliano Vallejos Santajuliana <vallejosgvs@gmail.com>
2020-10-04 13:35:34 -0300

    Modelo listo.

---
+++ SiADi/Modelo/Cargo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiADi.Modelo
+{
+    [Table("Cargos")]
+    public class Cargo : Base
+    {
+        public string Nombre { set; get; }
+        public double Salario { get; set; }
+        public DateTime Horario_entrada { set; get; }
+        public DateTime Horario_salida { set; get; }
+    }
+}
