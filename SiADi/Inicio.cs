2f052770ed60b95995ea934280f98503a3816e1c
Giuliano Vallejos Santajuliana <vallejosgvs@gmail.com>
2020-10-04 12:49:07 -0300

    Update Inicio.cs

--- SiADi/Inicio.cs
+++ SiADi/Inicio.cs
@@ -17,6 +17,7 @@ namespace SiADi
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new IngresoVista());
+            //cambio
         }
     }
 }
