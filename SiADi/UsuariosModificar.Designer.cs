1cf919d793af3446f209752f1baf4f3d57170928
paulibfernandez <69907060+paulibfernandez@users.noreply.github.com>
2020-10-04 14:55:29 -0300

    Fuente Usuarios Modificar

--- SiADi/UsuariosModificar.Designer.cs
+++ SiADi/UsuariosModificar.Designer.cs
@@ -40,11 +40,11 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Myriad Pro Cond", 20.25F);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
-            this.label1.Location = new System.Drawing.Point(177, 9);
+            this.label1.Location = new System.Drawing.Point(187, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.Size = new System.Drawing.Size(169, 30);
             this.label1.TabIndex = 2;
             this.label1.Text = " Modificar usuarios";
             // 
@@ -62,9 +62,10 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Myriad Pro Cond", 14.25F);
             this.textBox1.Location = new System.Drawing.Point(182, 59);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.Size = new System.Drawing.Size(157, 30);
             this.textBox1.TabIndex = 9;
             // 
             // dataGridView1
