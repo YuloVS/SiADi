0ba0bee4e873071de2da6636c4d7ced3b6c774d2
paulibfernandez <69907060+paulibfernandez@users.noreply.github.com>
2020-10-04 14:38:04 -0300

    Fuente cambiada Asistencias Registrar

--- SiADi/AsistenciasRegistrar.Designer.cs
+++ SiADi/AsistenciasRegistrar.Designer.cs
@@ -39,11 +39,11 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Myriad Pro Cond", 20.25F);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
             this.label1.Location = new System.Drawing.Point(168, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 25);
+            this.label1.Size = new System.Drawing.Size(175, 30);
             this.label1.TabIndex = 1;
             this.label1.Text = "Registrar asistencia";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -52,11 +52,11 @@
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Myriad Pro Cond", 20.25F);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
             this.label2.Location = new System.Drawing.Point(169, 339);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(201, 24);
+            this.label2.Size = new System.Drawing.Size(185, 30);
             this.label2.TabIndex = 43;
             this.label2.Text = "Escanee el codigo QR";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -67,7 +67,7 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Myriad Pro Cond", 12F);
             this.button1.ForeColor = System.Drawing.Color.LightGray;
             this.button1.Location = new System.Drawing.Point(173, 366);
             this.button1.Name = "button1";
