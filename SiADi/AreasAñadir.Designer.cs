3eac8f90e2ba5ec6614b5b21ec8e8cb02b15fb45
paulibfernandez <69907060+paulibfernandez@users.noreply.github.com>
2020-10-04 14:57:13 -0300

    Areas Añadir ubicacion

--- SiADi/AreasAñadir.Designer.cs
+++ SiADi/AreasAñadir.Designer.cs
@@ -46,11 +46,12 @@
             this.LabelAreasAñadir.AutoSize = true;
             this.LabelAreasAñadir.Font = new System.Drawing.Font("Myriad Pro Cond", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelAreasAñadir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
-            this.LabelAreasAñadir.Location = new System.Drawing.Point(197, 15);
+            this.LabelAreasAñadir.Location = new System.Drawing.Point(208, 9);
             this.LabelAreasAñadir.Name = "LabelAreasAñadir";
             this.LabelAreasAñadir.Size = new System.Drawing.Size(109, 30);
             this.LabelAreasAñadir.TabIndex = 2;
             this.LabelAreasAñadir.Text = "Nueva Área";
+            this.LabelAreasAñadir.Click += new System.EventHandler(this.LabelAreasAñadir_Click);
             // 
             // NombreAreasAñadir
             // 
@@ -58,7 +59,7 @@
             this.NombreAreasAñadir.AutoSize = true;
             this.NombreAreasAñadir.Font = new System.Drawing.Font("Myriad Pro Cond", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NombreAreasAñadir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
-            this.NombreAreasAñadir.Location = new System.Drawing.Point(115, 65);
+            this.NombreAreasAñadir.Location = new System.Drawing.Point(126, 65);
             this.NombreAreasAñadir.Name = "NombreAreasAñadir";
             this.NombreAreasAñadir.Size = new System.Drawing.Size(69, 26);
             this.NombreAreasAñadir.TabIndex = 4;
@@ -67,7 +68,7 @@
             // TextboxDescipcionAreasAñadir
             // 
             this.TextboxDescipcionAreasAñadir.Font = new System.Drawing.Font("Myriad Pro Cond", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextboxDescipcionAreasAñadir.Location = new System.Drawing.Point(133, 191);
+            this.TextboxDescipcionAreasAñadir.Location = new System.Drawing.Point(144, 191);
             this.TextboxDescipcionAreasAñadir.Margin = new System.Windows.Forms.Padding(2);
             this.TextboxDescipcionAreasAñadir.MaxLength = 100;
             this.TextboxDescipcionAreasAñadir.Name = "TextboxDescipcionAreasAñadir";
@@ -82,7 +83,7 @@
             this.DescripcionAreasAñadir.AutoSize = true;
             this.DescripcionAreasAñadir.Font = new System.Drawing.Font("Myriad Pro Cond", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DescripcionAreasAñadir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
-            this.DescripcionAreasAñadir.Location = new System.Drawing.Point(114, 165);
+            this.DescripcionAreasAñadir.Location = new System.Drawing.Point(125, 165);
             this.DescripcionAreasAñadir.Name = "DescripcionAreasAñadir";
             this.DescripcionAreasAñadir.Size = new System.Drawing.Size(92, 26);
             this.DescripcionAreasAñadir.TabIndex = 6;
@@ -122,7 +123,7 @@
             // TextboxNombreAreasAñadir
             // 
             this.TextboxNombreAreasAñadir.Font = new System.Drawing.Font("Myriad Pro Cond", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextboxNombreAreasAñadir.Location = new System.Drawing.Point(133, 88);
+            this.TextboxNombreAreasAñadir.Location = new System.Drawing.Point(144, 88);
             this.TextboxNombreAreasAñadir.Margin = new System.Windows.Forms.Padding(2);
             this.TextboxNombreAreasAñadir.MaxLength = 30;
             this.TextboxNombreAreasAñadir.Name = "TextboxNombreAreasAñadir";
